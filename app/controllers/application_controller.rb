a4db9c741d462aa6362ff23b025cd983db58bf16
yanarui <yanarui0602@icloud.com>
2025-07-04 23:21:59 +0900

    認証用メソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   SECRET_KEY = Rails.application.credentials.secret_key_base || ENV["SECRET_KEY_BASE"]
 
+  
   def encode_token(payload)
     JWT.encode(payload, SECRET_KEY)
   end
@@ -11,6 +12,21 @@ class ApplicationController < ActionController::API
       JWT.decode(token, SECRET_KEY, true, algorithm: "HS256")
     rescue JWT::DecodeError
       nil
+    end
+  end
+
+  private
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split(' ').last if header.present?
+    secret_key = Rails.application.credentials.secret_key_base
+
+    begin
+      decoded = JWT.decode(token, secret_key, true, algorithm: "HS256")
+      @current_user = User.find(decoded[0]["user_id"])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound => e
+      render json: { error: "認証エラー: #{e.message}" }, status: :unauthorized
     end
   end
 end
